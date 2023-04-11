aee533b668ecbf6d54acff7ec07a5e97c5bb9c46
Jarrod Mccoy <jar.mcccoy13@gmail.com>
2023-04-11 15:38:23 -0400

    revert

--- vue/src/components/ChatBot.vue
+++ vue/src/components/ChatBot.vue
@@ -2,7 +2,6 @@
   <div>
     <chat-greeting v-on:passNickname="getNickname($event)" />
     <p v-for="message in messages" v-bind:key="message.id">{{ message }}</p>
-
     <form v-if="showForm === true" v-on:submit.prevent="handleSubmit">
       <input
         id="chatEntry"
@@ -16,12 +15,10 @@
     <!-- <p>Current User: {{ this.$store.state.user.id }}</p> -->
   </div>
 </template>
-
 <script>
 /* import QuoteService from '../services/QuoteService.js' */
 import ResponseService from "../services/ResponseService.js";
 import ChatGreeting from "./ChatGreeting.vue";
-
 export default {
   components: {
     ChatGreeting,
@@ -35,27 +32,21 @@ export default {
       showForm: false
     };
   },
-
   // computed: {
   //     /* todo: come back to this and implement greeting based on nickname */
-
   //     // grabStoredNickName(){
   //     //     if (this.$store.state.user.nickname != null){
   //     //         return this.$store.user.nickname;
   //     //     }
-
   //     //      return null ;
   //     // },
-
   // },
-
   methods: {
     handleSubmit() {
       this.messages.push(this.userMessage);
       this.handleResponse(this.userMessage);
       this.userMessage = "";
     },
-
     handleResponse(userMessage) {
       ResponseService.getBotMessage(userMessage).then((response) => {
         const responseMessage = response.data.messageText;
@@ -67,7 +58,6 @@ export default {
       this.messages.push(nickname);
       this.messages.push('What can I help you with, ' + nickname + '?');
       this.showForm = true;
-      
     },
     // pageOpen(){
     //     let Greeting = '';
@@ -77,7 +67,6 @@ export default {
     //        let nickname = this.promptForNickname();
     //         Greeting = ` Hello ${nickname}`;
     //     }
-
     // this.messages.push(Greeting);
   },
   // promptForNickname(){
@@ -87,6 +76,5 @@ export default {
   // }
 };
 </script>
-
 <style scoped>
 </style>
