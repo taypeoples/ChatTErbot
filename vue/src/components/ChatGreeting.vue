57f054fa20afbc8c110a194d4ab6b9e79ac3a40b
Hamza Farah <farahamza42@outlook.com>
2023-04-11 10:07:48 -0400

    drive

---
+++ vue/src/components/ChatGreeting.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <p> 'Hello, what should I call you?'</p>
+      <form v-on:submit.prevent="handleNickname">
+      <input
+        id="chatEntry"
+        type="text"
+        class=""
+        v-model="nickname"
+        placeholder="Type your message here"
+      />
+      <button>Submit</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           
+        }
+        
+    }, 
+    mounted() {
+        
+            this.messages.push('Hello, what should I call you?');
+            const userSetNickname = window.prompt('Enter your nick-name');
+            return userSetNickname;
+        },
+      
+    
+     methods: {
+         handleNickname(nickname) {
+             this.$store.commit("SET_NICKNAME", nickname);
+         }
+     },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue/src/views/ChatBotView.vue
+++ vue/src/views/ChatBotView.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
+    <chat-greeting/>
       <chat-bot/>
+
   </div>
 </template>
 
 <script>
+
 import ChatBot from '../components/ChatBot.vue'
+import ChatGreeting from '../components/ChatGreeting.vue'
 export default {
   components: { 
-      ChatBot, 
+      ChatBot,
+    ChatGreeting, 
       },
 
 }
