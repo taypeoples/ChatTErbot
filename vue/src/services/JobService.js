f4a6e56e8d900539e317bb6b765f3d47564f62de
Jeremy Pratt <jerpratt1@gmail.com>
2023-04-14 15:12:30 -0400

    wittle work

---
+++ vue/src/services/JobService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default {
+
+    jobSearch() {
+        const options = {
+            method: 'GET',
+            url: 'https://jsearch.p.rapidapi.com/search',
+            params: { query: 'Junior developer in Ohio, USA', page: '1', num_pages: '1' },
+            headers: {
+                'X-RapidAPI-Key': '14fe3c00dfmsh014a33ce133cd06p1d9bc9jsna547623318d1',
+                'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+            }
+        };
+
+        return axios.get(options);
+    }
+}
