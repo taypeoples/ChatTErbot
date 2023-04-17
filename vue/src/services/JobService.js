9b664e9c87bcff95975afe70abf5cee8c990375a
Jeremy Pratt <jerpratt1@gmail.com>
2023-04-16 20:49:56 -0400

    putzing around

--- vue/src/services/JobService.js
+++ vue/src/services/JobService.js
@@ -4,12 +4,14 @@ import axios from "axios";
 
 export default {
 
-  getJobs(jobTitle, state){
+
+  //jsearch api good but may need to pay for 1 month so that we can do all the searches we need while working on this
+  getJobs(jobTitle, state) {
 
     const options = {
       method: 'GET',
       url: 'https://jsearch.p.rapidapi.com/search',
-      params: {query: `${jobTitle} in ${state}, USA`, page: '1', num_pages: '1'},
+      params: { query: `${jobTitle} in ${state}, USA`, page: '1', num_pages: '1' },
       headers: {
         'X-RapidAPI-Key': '14fe3c00dfmsh014a33ce133cd06p1d9bc9jsna547623318d1',
         'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
@@ -17,6 +19,7 @@ export default {
     };
 
     return axios.request(options)
-  }
+  },
+
 
 }
