a0d9f16d5b6ca963b0c7e2b497454f5debed756a
Jeremy Pratt <jerpratt1@gmail.com>
2023-04-18 19:06:50 -0400

    blank

--- java/src/main/java/com/techelevator/controller/MessageHelper.java
+++ java/src/main/java/com/techelevator/controller/MessageHelper.java
@@ -15,67 +15,83 @@ public class MessageHelper {
             keyWords[0] = "databases";
             keyWords[1] = "databasedesign";
             return keyWords;
-        } else if (strList.contains("resume") && (strList.contains("example") || strList.contains("examples"))) {
+        } else if (strList.contains("resume")
+                && (strList.contains("example") || strList.contains("examples") || strList.contains("link") || strList.contains("links"))) {
             keyWords[0] = "resume";
             keyWords[1] = "links";
             return keyWords;
-        } else if (strList.contains("resume") && strList.contains("parts")) {
+        } else if (strList.contains("resume")
+                && strList.contains("parts")) {
             keyWords[0] = "resume";
             keyWords[1] = "parts";
             return keyWords;
-        } else if (strList.contains("interview") && strList.contains("prep")) {
+        } else if (strList.contains("interview")
+                && strList.contains("prep")) {
             keyWords[0] = "interview";
             keyWords[1] = "prep";
             return keyWords;
-        } else if (strList.contains("interview") && strList.contains("follow")) {
+        } else if (strList.contains("interview")
+                && strList.contains("follow")) {
             keyWords[0] = "interview";
             keyWords[1] = "follow";
             return keyWords;
-        } else if (strList.contains("interview") && strList.contains("outfit")) {
+        } else if (strList.contains("interview")
+                && strList.contains("outfit")) {
             keyWords[0] = "interview";
             keyWords[1] = "outfit";
             return keyWords;
-        } else if (strList.contains("interview") && strList.contains("star")) {
+        } else if (strList.contains("interview")
+                && strList.contains("star")) {
             keyWords[0] = "interview";
             keyWords[1] = "star";
             return keyWords;
-        } else if (strList.contains("cover") && strList.contains("parts")) {
+        } else if (strList.contains("cover")
+                && strList.contains("parts")) {
             keyWords[0] = "cover";
             keyWords[1] = "parts";
             return keyWords;
-        } else if (strList.contains("cover") && strList.contains("links")) {
+        } else if (strList.contains("cover")
+                && strList.contains("links")) {
             keyWords[0] = "cover";
             keyWords[1] = "links";
             return keyWords;
-        } else if (strList.contains("social") && strList.contains("linkedin")) {
+        } else if (strList.contains("social")
+                && strList.contains("linkedin")) {
             keyWords[0] = "social";
             keyWords[1] = "link";
             return keyWords;
-        } else if (strList.contains("social") && strList.contains("huntr")) {
+        } else if (strList.contains("social")
+                && strList.contains("huntr")) {
             keyWords[0] = "social";
             keyWords[1] = "hunt";
             return keyWords;
-        } else if (strList.contains("technical interview") && strList.contains("prep")) {
+        } else if (strList.contains("technical interview")
+                && strList.contains("prep")) {
             keyWords[0] = "technical";
             keyWords[1] = "preparation";
             return keyWords;
-        } else if (strList.contains("technical interview") && strList.contains("explanation")) {
+        } else if (strList.contains("technical interview")
+                && strList.contains("explanation")) {
             keyWords[0] = "technical";
             keyWords[1] = "explanation";
             return keyWords;
-        } else if (strList.contains("networking") && strList.contains("prep")) {
+        } else if (strList.contains("networking")
+                && strList.contains("prep")) {
             keyWords[0] = "networking";
             keyWords[1] = "prep";
             return keyWords;
-        } else if (strList.contains("networking") && strList.contains("events")) {
+        } else if (strList.contains("networking")
+                && strList.contains("events")) {
             keyWords[0] = "networking";
             keyWords[1] = "events";
             return keyWords;
-        } else if (strList.contains("matchmaking") && strList.contains("overview")) {
+        } else if (strList.contains("matchmaking")
+                && strList.contains("overview")) {
             keyWords[0] = "matchmaking";
             keyWords[1] = "overview";
             return keyWords;
-        } else if (strList.contains("matchmaking") && strList.contains("prep")) {
+        } else if (strList.contains("matchmaking")
+                && strList.contains("prep")) {
             keyWords[0] = "matchmaking";
             keyWords[1] = "prep";
             return keyWords;
@@ -97,6 +113,10 @@ public class MessageHelper {
             return keyWords;
         } else if (strList.contains("networking")) {
             keyWords[0] = "networking";
+            keyWords[1] = "default";
+            return keyWords;
+        } else if (strList.contains("social")) {
+            keyWords[0] = "social";
             keyWords[1] = "default";
             return keyWords;
         } else if (strList.contains("matchmaking")) {
