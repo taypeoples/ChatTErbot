00f16be2a50b13f8db0ba960c49fcb7b0fb7425e
Taylor Peoples <mtaylorpeoples@gmail.com>
2023-04-19 13:54:48 -0400

    changes to job search

--- vue/src/components/JobSearch.vue
+++ vue/src/components/JobSearch.vue
@@ -1,58 +1,94 @@
 <template>
-  <div>
-  <div class="job-search-form">
-    <div class="left">
-      <div class="overlay">
-        <h1>Job Search</h1>
-        <span>
-          <p>Find your dream job today</p>
-        </span>
+  <div class = "jsearch">
+    <div class="job-search-form">
+      <div class="left">
+        <div class="overlay">
+          <h1>Job Search</h1>
+          <span>
+            <p>Find your dream job today</p>
+          </span>
+        </div>
+      </div>
+      <div class="right">
+        <h5>Search for Jobs</h5>
+        <form>
+          <div>
+            <label for="jtitle" class="text-center"></label><br />
+            <input
+              v-model="jobTitle"
+              type="text"
+              id="jtitle"
+              name="jtitle"
+              placeholder="Job Title"
+            />
+          </div>
+          <div>
+            <label for="location" class="text-center"></label><br />
+            <input
+              v-model="location"
+              type="text"
+              id="location"
+              name="location"
+              placeholder="Location"
+            />
+          </div>
+          <div>
+            <p>Job Type:</p>
+            <div class="checkbox-group">
+              <input
+                v-model="jobType"
+                type="checkbox"
+                id="ftime"
+                name="ftime"
+                value="Full-time"
+              />
+              <label for="ftime">Full-time</label><br />
+              <input
+                v-model="jobType"
+                type="checkbox"
+                id="ptime"
+                name="ptime"
+                value="Part-time"
+              />
+              <label for="ptime">Part-time</label><br />
+              <input
+                v-model="jobType"
+                type="checkbox"
+                id="internship"
+                name="internship"
+                value="Internship"
+              />
+              <label for="internship">Internship</label><br />
+              <input
+                v-model="jobType"
+                type="checkbox"
+                id="contract"
+                name="contract"
+                value="Contractor"
+              />
+              <label for="contract">Contractor</label><br /><br />
+            </div>
+          </div>
+          <div>
+            <label for="remote">Remote-Only:</label>
+            <input v-model="remote" type="checkbox" id="remote" />
+          </div>
+        </form>
+      </div>
+      <div class="right2">
+        <button v-on:click="populateJobsArray">Search for jobs</button>
       </div>
     </div>
-    <div class="right">
-      <h5>Search for Jobs</h5>
-      <form>
-        <div>
-          <label for="jtitle" class="text-center"></label><br />
-          <input v-model="jobTitle" type="text" id="jtitle" name="jtitle" placeholder="Job Title" />
-        </div>
-        <div>
-          <label for="location" class="text-center"></label><br />
-          <input v-model="location" type="text" id="location" name="location" placeholder="Location" />
-        </div>
-        <div>
-          <p>Job Type:</p>
-          <div class="checkbox-group">
-            <input v-model="jobType" type="checkbox" id="ftime" name="ftime" value="Full-time" />
-            <label for="ftime">Full-time</label><br>
-            <input v-model="jobType" type="checkbox" id="ptime" name="ptime" value="Part-time" />
-            <label for="ptime">Part-time</label><br>
-            <input v-model="jobType" type="checkbox" id="internship" name="internship" value="Internship" />
-            <label for="internship">Internship</label><br>
-            <input v-model="jobType" type="checkbox" id="contract" name="contract" value="Contractor" />
-            <label for="contract">Contractor</label><br><br>
-          </div>
-        </div>
-        <div>
-          <label for="remote">Remote-Only:</label>
-          <input v-model="remote" type="checkbox" id="remote" />
-        </div>
-      </form>
-    </div>
-    <div class="right2">
-      <button v-on:click="populateJobsArray">
-       Search for jobs
-      </button>
-    </div>
-  </div>
-  <br />
-  <div class="job-cards-container">
+    <br />
+    <div class="job-cards-container">
       <div v-for="job in jobs" :key="job.id" class="job-card">
         <div class="card-header">{{ job.employer_name }}</div>
         <div class="card-body">
           <h5 class="card-title">{{ job.job_title }}</h5>
           <pre class="card-text">{{ job.job_description }}</pre>
-          <a :href="job.job_apply_link" target="_blank" class="btn-primary">Apply</a>      
+          <a :href="job.job_apply_link" target="_blank" class="btn-primary"
+            >Apply</a
+          >
         </div>
         <div class="card-footer text-muted">{{ job.job_location }}</div>
       </div>
@@ -70,7 +106,7 @@ export default {
       location: "",
       jobType: [],
       remote: false,
-      updatedDescription:"",
+      updatedDescription: "",
     };
   },
 
@@ -89,15 +125,18 @@ export default {
         this.remote
       ).then((response) => {
         this.jobs = response.data.data;
-
       });
     },
-    
   },
 };
 </script>
 
 <style scoped>
+.jsearch {
+  background-image: url('C:\Users\Student\source\repos\pair programming\team-quebec\vue\pictures\searchbg.png');
+  
+}
+
 .job-cards {
   font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
   text-align: center;
@@ -105,6 +144,7 @@ export default {
 
 .job-card {
   margin: 10px;
+  background-color: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   border-radius: 5px;
@@ -114,50 +154,45 @@ export default {
   max-height: 600px;
   min-height: 600px;
   min-width: 600px;
- 
 }
 
 .job-card:hover {
   box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 
-.card-body{
+.card-body {
   padding: 10px;
 }
 
-
-pre{
+pre {
   font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
   white-space: pre-wrap;
   padding: 10px;
 }
 
-
 .btn-primary {
-	box-shadow: 0px 0px 0px 2px #9fb4f2;
-	background:linear-gradient(to bottom, #7892c2 5%, #476e9e 100%);
-	background-color:#7892c2;
-	border-radius:10px;
-	border:1px solid #4e6096;
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:19px;
-	padding:12px 37px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #283966;
-
+  box-shadow: 0px 0px 0px 2px #9fb4f2;
+  background: linear-gradient(to bottom, #7892c2 5%, #476e9e 100%);
+  background-color: #7892c2;
+  border-radius: 10px;
+  border: 1px solid #4e6096;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 19px;
+  padding: 12px 37px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #283966;
 }
 .btn-primary:hover {
-	background:linear-gradient(to bottom, #476e9e 5%, #7892c2 100%);
-	background-color:#476e9e;
+  background: linear-gradient(to bottom, #476e9e 5%, #7892c2 100%);
+  background-color: #476e9e;
 }
 .btn-primary:active {
-	position:relative;
-	top:1px;
+  position: relative;
+  top: 1px;
 }
-
 
 .card-header {
   background-color: #3b5998;
@@ -173,9 +208,9 @@ pre{
   text-align: center;
 }
 
- /* Form Styling */
+/* Form Styling */
 body {
-  background-image: linear-gradient(135deg, #FAB2FF 10%, #1904E5 100%);
+  background-image: linear-gradient(135deg, #fab2ff 10%, #1904e5 100%);
   background-size: cover;
   background-repeat: no-repeat;
   background-attachment: fixed;
@@ -192,7 +227,7 @@ body {
 }
 
 button {
-  background-color:#3b5998;
+  background-color: #3b5998;
   color: white;
   padding: 10px 20px;
   border: none;
@@ -222,7 +257,6 @@ button:hover {
   align-items: stretch;
   justify-content: space-between;
   box-shadow: 0 0 20px 6px #2759c585;
-  
 }
 
 .job-search-form .overlay {
@@ -236,14 +270,12 @@ button:hover {
   width: 0px;
 }
 
-
 @media (max-width: 980px) {
   .job-search-form {
     flex-flow: wrap;
     text-align: center;
     align-content: center;
     align-items: center;
-  
   }
 }
 
@@ -252,11 +284,10 @@ button:hover {
 }
 
 .job-search-form .left {
-  color: #FFFFFF;
+  color: #ffffff;
   background-size: 100%;
   background-repeat: no-repeat;
-  background-image: url('/pictures/Capture3.PNG');
-  
+  background-image: url("/pictures/Capture3.PNG");
 }
 
 .job-search-form .left .overlay {
@@ -280,10 +311,9 @@ button:hover {
   font-weight: 900;
 }
 
-
 .job-search-form .left .overlay span a {
   background: #3b5998;
-  color: #FFFFFF;
+  color: #ffffff;
   margin-top: 10px;
   padding: 14px 50px;
   border-radius: 100px;
@@ -302,19 +332,18 @@ button:hover {
 }
 
 @media (max-width: 480px) {
-  .job-search-form{
+  .job-search-form {
     height: 40%;
   }
   .job-search-form .right {
     width: 100%;
-    
   }
 }
 
-  .job-search-form .right h5 {
+.job-search-form .right h5 {
   font-size: 5vmax;
   line-height: 1;
-  background-image: url('/pictures/Capture3.PNG');
+  background-image: url("/pictures/Capture3.PNG");
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
@@ -322,10 +351,9 @@ button:hover {
   margin-bottom: 30px;
 }
 
-
 .job-search-form .right p {
   font-size: 14px;
-  color: #B0B3B9;
+  color: #b0b3b9;
 }
 
 .job-search-form .right .inputs {
@@ -335,7 +363,7 @@ button:hover {
 .job-search-form .right input[type="text"] {
   border: none;
   outline: none;
-  border-bottom: 2px solid #B0B3B9;
+  border-bottom: 2px solid #b0b3b9;
   width: 100%;
   padding: 10px 0;
   font-size: 18px;
@@ -343,14 +371,13 @@ button:hover {
 }
 .job-search-form .right input[type="submit"] {
   background: #3b5998;
-  color: #FFFFFF;
+  color: #ffffff;
   border: none;
   outline: none;
   padding: 10px 20px;
   border-radius: 50px;
   font-size: 18px;
   cursor: pointer;
-  box-shadow: 0 3px 6px
+  box-shadow: 0 3px 6px;
 }
-
 </style>
